8a0480d2fcfc72ac53bc43a1f0fbf91ba0a322eb
R0D7 <121826354+R0D7@users.noreply.github.com>
2023-03-07 18:43:31 +0100

    Update speech_to_text.py

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,5 +1,5 @@
 import openai
-def whisper_recognition():
-    transcribe = openai.Audio.transcribe("whisper-1", open("path\\speech.wav", "rb"))
+def whisper_recognition(speech_path):
+    transcribe = openai.Audio.transcribe("whisper-1", open(speech_path, "rb"))
     print("User : ", transcribe["text"])
     return transcribe["text"]
